617dfcd85aa6765c210a66c07d478828df34becf
Kate <kate.dovzhenk@gmail.com>
2025-04-26 16:30:13 -0400

    hw is done

---
+++ src/basic/1.ts
@@ -0,0 +1,7 @@
+const age: number = 50;
+const username: string = "Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback3: (a: number) => number = (a) => {
+  return 100 + a;
+};
